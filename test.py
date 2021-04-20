2ba4f574c46c6b132df1a0a812c6bdbeaeffd1f7
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-20 17:44:12 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+"Hello, Python!"
+
+print(int(0x1D))
+print(hex(45))
+print(oct(0o23))
+print(bin(13))
+print(int(0b11011))
+
+a=5
+print(id(a))
+print(type(a))
+a=5.5
+print(id(a))
+print(type(a))
+a='5'
+print(id(a))
+print(type(a))
