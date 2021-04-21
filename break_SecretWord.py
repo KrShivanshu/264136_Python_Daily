55641e7ee328066c007b4cf9a7474fe4c0bd6ead
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-22 01:41:29 +0530

    Create break_SecretWord.py

---
+++ break_SecretWord.py
@@ -0,0 +1,7 @@
+word = input("Enter a word: ")
+
+while word!="chupacabra":
+    word = input("Enter a word: ")
+    if word == "chupacabra":
+        print("You've successfully left the loop.")
+        break
