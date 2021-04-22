21b86c94223f1faecca2532673d0d2e1fcc45fce
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-22 17:37:08 +0530

    Update List.py

--- List.py
+++ List.py
@@ -34,3 +34,23 @@ print("\nNew list content:", numbers)  # Printing current list content.
 numbers = [111, 7, 2, 1]
 print(numbers[-1])
 print(numbers[-2])
+
+
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+# Step 1: write a line of code that prompts the user
+# to replace the middle number with an integer number entered by the user.
+
+# Step 2: write a line of code that removes the last element from the list.
+
+# Step 3: write a line of code that prints the length of the existing list.
+
+print(hat_list)
+
+hat_list[2] = int(input("Enter a number: "))
+print(hat_list)
+
+del(hat_list[-1])
+print(hat_list)
+
+print(len(hat_list))
