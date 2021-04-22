60f2869383e9442e472acbe681b3133a4df8a950
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-22 18:13:41 +0530

    Update ListMethods.py

--- ListMethods.py
+++ ListMethods.py
@@ -41,3 +41,36 @@ for i in range(len(my_list)):
 
 print(total)
 
+
+###
+
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+# step 3
+for i in range(2):
+    beatles.append(input("Enter a word: "))
+print("Step 3:", beatles)
+
+# step 4
+del(beatles[-1])
+del(beatles[-1])
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0,"Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
+
+
+###
