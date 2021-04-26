03e76c2d4c227646e9e062eb23c4a8e3f56450b8
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-27 05:24:28 +0530

    Create listTest.py

---
+++ listTest.py
@@ -0,0 +1,18 @@
+lists = [1,2]
+#for v in range(2):
+#    lists.insert(-1,lists[v])
+
+print(lists)
+
+num =  lists
+del num[:]
+print(num)
+print(lists)
+
+def func(a=2,b=3):
+     return b**a
+
+print(func(b=2))
+
+lists = [i for i in range(-1,-2)]
+print(5 not== 6)
