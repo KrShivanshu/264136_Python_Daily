4c7c12ca46e81a2c51a342474f069e0c58f2dae0
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-29 23:34:28 +0530

    Update listTest.py

--- listTest.py
+++ listTest.py
@@ -1,4 +1,7 @@
 lists = [1,2,3,4,5]
+print(lists[0:-1])
+for v in enumerate(lists):
+     print(v)
 for v in range(2):
      lists.insert(-1,lists[v])
 
@@ -16,3 +19,45 @@ print(func(b=2))
 
 lists = [i for i in range(-1,-2)]
 print(5 == 6)
+
+#####
+
+# Generators
+def countdown():
+     i=5
+     while i>0:
+          yield i
+          i-=1
+
+for i in countdown():
+     print(i)
+
+# decorator
+def decor(func):
+     def wrap():
+          print("=====")
+          func()
+          print("=====")
+     return wrap
+
+@decor
+def print_text():
+     print("hello world!")
+
+#print_text = decor(print_text)
+print_text()
+
+# recursion
+def fib(x):
+     if x == 0 or x == 1:
+          return 1
+     else: 
+          return fib(x-1) + fib(x-2)
+print(fib(4))
+
+#itertools
+from itertools import product, permutations
+
+letters = ("a","b")
+print(list(product(letters,range(2))))
+print(list(permutations(letters)))
